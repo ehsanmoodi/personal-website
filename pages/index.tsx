13defd30563dc6eb6b30e5876da11bc6e4600249
ehsanmoodi <ehsanmoodi90@gmail.com>
2021-03-22 01:29:38 +0430

    tadd typescript and tailwindcss support

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from "next";
+import Head from 'next/head';
+// import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Ehsan Moodi</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      
+    </main>
+
+    {/* <footer className={styles.footer}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      </a>
+    </footer> */}
+  </div>
+)
+
+export default Home;
